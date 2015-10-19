2ced20a55d3fd6ead14c0428f0f9af47871a5cde
zyfjeff <445188383@qq.com>
2015-10-19 21:14:47 +0800

    右值引用相关基础

---
+++ rvalue.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass
+{
+private:
+	static int s_Counter;
+	int* m_Member{&s_Counter};
+public:
+	MyClass()
+	{
+		++(*m_Member);
+		cout << "Constructing: " << GetValue()<< endl;
+	}
+
+	~MyClass()
+	{
+		--(*m_Member);
+		m_Member = nullptr;
+		cout << "Destructing: " << s_Counter << endl;
+	}
+	MyClass(const MyClass& rhs)
+			:m_Member{rhs.m_Member}
+	{
+		++(*m_Member);
+		cout << "Copy: " << GetValue() << endl;
+	}
+
+
+	int GetValue()const
+	{
+		return *m_Member;
+	}
+};
+
+int MyClass::s_Counter{0}; //静态变量初始化
+
+MyClass CopyMyClass(MyClass parameter)
+{
+	return parameter;
+}
+
+int main()
+{
+	auto object1 = MyClass();
+	cout << object1.GetValue() << endl;
+	{
+		auto object2 = MyClass();
+		cout << object2.GetValue() << endl;
+	}
+	auto object3 = MyClass();
+	cout << object3.GetValue() << endl;
+
+	auto object4 = CopyMyClass(object3); //发生了两次拷贝动作,如果引入移动语义可以避免临时对象到object4的拷贝
+	
+	return 0;
+}
+
+
