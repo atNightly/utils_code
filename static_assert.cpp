20a655cf6039257608fc3a9c292d610711fb23f2
zyfjeff <445188383@qq.com>
2015-10-19 20:22:00 +0800

    atomic测试

---
+++ static_assert.cpp
@@ -0,0 +1,14 @@
+#include <boost/static_assert.hpp>
+#include <inttypes.h>
+#include <iostream>
+#include <cstdio>
+#include "timestamp.h"
+
+//编译器断言
+BOOST_STATIC_ASSERT(sizeof(int64_t) == 8);
+
+
+int main()
+{
+}
+
