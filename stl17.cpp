6141c0766732ea362452ed9516969149ed2381ce
zyfjeff <445188383@qq.com>
2015-10-26 08:05:54 +0800

    add

--- stl17.cpp
+++ stl17.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 class SetObject;
 
+//给hash容器做模板特化
 namespace std
 {
 	template<>
 	class hash<SetObject>
 	{
 		public:
-			template<typename... Args>
+			template<typename... Args> //可变的模板参数
 			size_t operator()(Args&&... setObject)const
 			{
 				return hash<string>()((forward<Args...>(setObject...)).GetName());
