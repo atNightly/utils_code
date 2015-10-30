e80f2fb424a45ede3ab63992536fc3b4ffa10bc0
zyfjeff <445188383@qq.com>
2015-10-30 08:04:11 +0800

    unique_ptr

---
+++ mm1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+class MyClass
+{
+		private:
+				int m_Number{0};
+		public:
+				MyClass(int value)
+						:m_Number{ value }
+				{
+					
+				}
+				~MyClass()
+				{
+					cout << "Destoring " << m_Number << endl;
+				}
+
+				void operator=(const int value)
+				{
+					m_Number = value;
+				}
+
+				int GetNumber()const
+				{
+					return m_Number;
+				}
+};
+
+using UniqueMyclass = unique_ptr<MyClass>;
+
+void CreateUniqueObject(UniqueMyclass& referenceToUniquePtr)
+{
+	UniqueMyclass uniquemyclass{static_cast<UniqueMyclass>(new MyClass(10))};
+	cout << hex << showbase;
+	cout << "Address in uniqueMyclass" << uniquemyclass.get() << endl;
+	referenceToUniquePtr.swap(uniquemyclass);
+	cout << "Address in uniqueMyclass" << uniquemyclass.get() << endl;
+}
+
+int main(int argc,char* argv[])
+{
+	cout << "Begin Main" << endl;
+	UniqueMyclass  uniqueMyclass;
+	CreateUniqueObject(uniqueMyclass);
+	cout << "Address in main's UniqueMyclass" << uniqueMyclass.get() << endl;
+	cout << dec << noshowbase << "Back in Main" << endl;
+	return 0;
+}
